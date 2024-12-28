cc1bf890b1400f5405f3785585a567a3f334da58
Dennis <bantel200@googlemail.com>
2024-12-28 16:42:50 +0100

    create with json of all plz

--- plzService.py
+++ plzService.py
@@ -1,10 +1,13 @@
 from models import PLZ_PVModel
+import json
 class PLZService:
     def __init__(self):
         self.model = PLZ_PVModel()
 
     def create(self, request):
-        return self.model.create(request["PLZ"], request["PV"])
+        for plz in request["plzs"]:
+            self.model.create(plz["PLZ"], plz["PV"])
+        return "OK"
 
     def get_all(self):
         return self.model.get_all()
